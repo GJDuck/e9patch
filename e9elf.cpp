GJDuck/e9patch

commit e716ce1e3b0fe2ca99b844c9b7758ab6542d7059

Make e9loader search all of /proc/self/map_files/
Previously, it would give up if there were too
many entries.  This was a problem for shared
objects with many dependencies.

e9loader.cpp
@@ -343,44 +343,49 @@ static NO_INLINE int e9binary(char *path_buf)
     if (fd < 0)
         return fd;
     uint8_t buf[BUFSIZ];
-    int len = e9getdents64(fd, (struct linux_dirent64 *)buf, BUFSIZ);
-    e9close(fd);
-    if (len < 0)
-        return len;
 
     // Step (2): Search for an entry corresponding to the enclosing function.
     intptr_t code = (intptr_t)&e9binary;
     bool found = false;
-    for (int i = 0; !found && i < len; )
+    while (!found)
     {
-        struct linux_dirent64 *entry = (struct linux_dirent64 *)(buf + i);
-        i += entry->d_reclen;
-        if (entry->d_type != DT_LNK)
-            continue;
-        const char *name = entry->d_name;
-        intptr_t lo = e9hexstr2num(&name);
-        if (code < lo)
-            continue;
-        intptr_t hi = e9hexstr2num(&name);
-        if (code <= hi)
+        int len = e9getdents64(fd, (struct linux_dirent64 *)buf, sizeof(buf));
+        if (len < 0)
         {
-            int j;
-            for (j = 0; mapsname[j] != '\0'; j++)
-                path_buf[j] = mapsname[j];
-            name = entry->d_name;
-            for (int k = 0; name[k] != '\0'; k++)
-                path_buf[j++] = name[k];
-            path_buf[j] = '\0';
-            found = true;
+            e9close(fd);
+            return len;
+        }
+
+        for (int i = 0; !found && i < len; )
+        {
+            struct linux_dirent64 *entry = (struct linux_dirent64 *)(buf + i);
+            i += entry->d_reclen;
+            if (entry->d_type != DT_LNK)
+                continue;
+
+            const char *name = entry->d_name;
+            intptr_t lo = e9hexstr2num(&name);
+            if (code < lo)
+                continue;
+            intptr_t hi = e9hexstr2num(&name);
+            if (code <= hi)
+            {
+                int j;
+                for (j = 0; mapsname[j] != '\0'; j++)
+                    path_buf[j] = mapsname[j];
+                name = entry->d_name;
+                for (int k = 0; name[k] != '\0'; k++)
+                    path_buf[j++] = name[k];
+                path_buf[j] = '\0';
+                found = true;
+            }
         }
     }
-    if (!found)
-        return ENOENT;
 
     // Step (3): Read the link.
     // Note: this is necessary since Linux does not allow the path to be
     //       opened directy.
-    len = e9readlink(path_buf, path_buf, BUFSIZ);
+    int len = e9readlink(path_buf, path_buf, BUFSIZ);
     if (len < 0)
         return len;
     path_buf[len] = '\0';

commit 2302884266ba42e72ec78ba085c6b47450c6a731

Fix Chrome rewriting.
Chrome was no longer working---even if we do not
rewrite any instructions.

It turns out that e716ce1 introduced a bug where
an fd was not closed, and apparently Chrome will
detect this and refuse to run.

e9elf.cpp
@@ -546,6 +546,7 @@ static size_t emitLoader(const RefactorSet &refactors,
         0x5e,                           // popq %rsi
         0x5a,                           // popq %rdx
         0x59,                           // popq %rcx
+        0x41, 0x58,                     // popq %r8
         0x41, 0x59,                     // popq %r9
         0x41, 0x5a,                     // popq %r10
         0x41, 0x5b,                     // popq %r11
@@ -553,7 +554,6 @@ static size_t emitLoader(const RefactorSet &refactors,
         0x41, 0x5d,                     // popq %r13
         0x41, 0x5e,                     // popq %r14
         0x41, 0x5f,                     // popq %r15
-        0x41, 0x58,                     // popq %r8
     };
     memcpy(data + size, restore_state, sizeof(restore_state));
     size += sizeof(restore_state);

e9loader.cpp
@@ -381,10 +381,11 @@ static NO_INLINE int e9binary(char *path_buf)
             }
         }
     }
+    e9close(fd);
 
     // Step (3): Read the link.
     // Note: this is necessary since Linux does not allow the path to be
-    //       opened directy.
+    //       opened directly.
     int len = e9readlink(path_buf, path_buf, BUFSIZ);
     if (len < 0)
         return len;