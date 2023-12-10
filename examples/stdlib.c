811642cf744ba1726fc34851dc44f4c4df436ce7
GJDuck <gregory@comp.nus.edu.sg>
2023-12-10 10:50:17 +0800

    Add file descriptor type macros to stdlib.c

--- examples/stdlib.c
+++ examples/stdlib.c
@@ -649,6 +649,15 @@ typedef int key_t;
 #define S_ISGID                         02000
 #define S_ISVTX                         01000
 
+#define S_IFMT                          0170000
+#define S_IFSOCK                        0140000
+#define S_IFLNK                         0120000
+#define S_IFREG                         0100000
+#define S_IFBLK                         0060000
+#define S_IFDIR                         0040000
+#define S_IFCHR                         0020000
+#define S_IFIFO                         0010000
+
 #define O_ACCMODE                       00000003
 #define O_RDONLY                        00000000
 #define O_WRONLY                        00000001
