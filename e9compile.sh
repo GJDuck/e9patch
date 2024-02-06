479df41f06606ff72ec5e4013cf021fdbf8c052b
Milan Hauth <milahu@gmail.com>
2024-02-06 12:45:27 +0100

    e9compile: fix colors

--- e9compile.sh
+++ e9compile.sh
@@ -17,11 +17,11 @@
 
 if [ -t 1 ]
 then
-    RED="\033[31m"
-    GREEN="\033[32m"
-    YELLOW="\033[33m"
-    BOLD="\033[1m"
-    OFF="\033[0m"
+    RED=$'\033[31m'
+    GREEN=$'\033[32m'
+    YELLOW=$'\033[33m'
+    BOLD=$'\033[1m'
+    OFF=$'\033[0m'
 else
     RED=
     GREEN=
