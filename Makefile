GJDuck/e9patch

commit 9f42bf2c2198ac149acc9b50f0641189b6a5d9ff

Reimplement the E9Patch loader.
In the new version, most of the code is written
in C++ rather than generated machine code.  This
is a much better design, and makes it easier to
extend.

The new loader should also be faster.

Makefile
@@ -52,11 +52,11 @@ tool.debug: e9tool.o
         -Wl,--export-dynamic -ldl
 
 loader:
-	$(CXX) -std=c++11 -Wall -fno-stack-protector -fpie -Os -c \
-        src/e9patch/e9loader.cpp
-	$(CXX) -nostdlib -o e9loader.out e9loader.o -Wl,--entry=_entry
-	objcopy --dump-section .text=e9loader.bin e9loader.out
-	xxd -i e9loader.bin > src/e9patch/e9loader.c
+	$(CXX) -std=c++11 -Wall -fno-stack-protector -Wno-unused-function -fPIC \
+        -mno-mmx -mno-sse -mno-avx -mno-avx2 -mno-avx512f -msoft-float \
+        -Os -c src/e9patch/e9loader_elf.cpp
+	$(CXX) -pie -nostdlib -o e9loader_elf.bin e9loader_elf.o -T e9loader.ld
+	xxd -i e9loader_elf.bin > src/e9patch/e9loader_elf.c
 
 src/e9patch/e9alloc.o: CXXFLAGS += -Wno-unused-function
 

test/regtest/Makefile
@@ -9,6 +9,7 @@ all:
 	../../e9compile.sh inst.c -I ../../examples/ -D NO_GLIBC
 	../../e9compile.sh patch.cpp -I ../../examples/ -D NO_GLIBC
 	../../e9compile.sh dl.c -I ../../examples/
+	../../e9compile.sh init.c -I ../../examples/ -D NO_GLIBC
 	g++ -std=c++11 -pie -fPIC -o regtest regtest.cpp -O2
 
 clean:

e9loader.ld
@@ -1,0 +1,5 @@
+SECTIONS
+{
+	.text : { *(.text .rodata .rodata.*) }
+}
+OUTPUT_FORMAT(binary)

src/e9patch/e9loader.cpp
@@ -1,425 +1,0 @@
-/*
- * e9loader.cpp
- * Copyright (C) 2021 National University of Singapore
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to
- * deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
- *
- * NOTE: As a special exception, this file is under the MIT license.  The
- *       rest of the E9Patch/E9Tool source code is under the GPLv3 license.
- */
-
-#include <cerrno>
-#include <csignal>
-#include <cstdint>
-#include <cstdlib>
-
-#include <dirent.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <unistd.h>
-
-#include "e9loader.h"
-
-#define NO_INLINE    __attribute__((__noinline__))
-#define NO_RETURN   __attribute__((__noreturn__))
-
-#define STRING(x)   STRING_2(x)
-#define STRING_2(x) #x
-
-#define BUFSIZ      8192
-
-static NO_INLINE int e9binary(char *path_buf);
-
-extern const char mapsname[];
-extern const char maps_err_str[];
-extern const char open_err_str[];
-extern const char mmap_err_str[];
-extern const char common_err_str[];
-
-extern "C"
-{
-    int e9entry(void);
-    NO_INLINE NO_RETURN void e9error(const char *err_str, int err);
-}
-
-/*
- * Directory entry structure.
- */
-struct linux_dirent64
-{
-    ino64_t        d_ino;
-    off64_t        d_off;
-    unsigned short d_reclen;
-    unsigned char  d_type;
-    char           d_name[];
-};
-
-asm (
-    /*
-     * Loader base stored here:
-     */
-    ".Lbase:\n"
-    ".quad 0x0\n"
-
-    /*
-     * Entry into stage #1 (.Lbase+8).  We:
-     *  (1) save the state.
-     *  (2) call e9entry()
-     *  (3) setup stage #2 parameters
-     *  (4) jump to stage #2
-     */
-    ".globl _entry\n"
-    ".type _entry,@function\n"
-    "_entry:\n"
-
-    // (0) debug point
-    "\tnop\n"
-
-    // (1) save the state
-    "\tpushq %r15\n"
-    "\tpushq %r14\n"
-    "\tpushq %r13\n"
-    "\tpushq %r12\n"
-    "\tpushq %r11\n"
-    "\tpushq %r10\n"
-    "\tpushq %r9\n"
-    "\tpushq %r8\n"
-    "\tpushq %rcx\n"
-    "\tpushq %rdx\n"
-    "\tpushq %rsi\n"
-    "\tpushq %rdi\n"
-
-    // (2) call e9entry()
-    "\tcallq e9entry\n"                     // Call main entry routine
-
-    // (3) setup stage #2 parameters
-    // Stage #2 expects certain parameters to be in specific registers:
-    "\tmov %eax, %r8d\n"                    // fd into %r8
-    "\tleaq .Lbase(%rip), %r12\n"           // .Lbase into %r12
-    "\tmov (%r12), %rdx\n"                  // Loader base into %rdx
-    "\tsubq %rdx,%r12\n"                    // ELF base into %r12
-    "\tmov $9, %r13d\n"                     // SYS_MMAP into %r13
-    "\tleaq .LMMAP_ERROR(%rip), %r14\n"     // mmap error handler into %r14
-
-    // (4) jump to stage #2
-    // Stage #2 will be placed at the end of the (.text) section.
-    "\tjmp __etext\n"
-    
-    /*
-     * The following defines the read-only data used by the loader.
-     * Note that we define the data as executable code to keep everything
-     * in the (.text) section.
-     */
-    ".global mapsname\n"
-    ".type mapsname,@function\n"
-    "mapsname:\n"
-    ".ascii \"/proc/self/map_files/\"\n"
-    ".byte 0x00\n"
-
-    ".globl maps_err_str\n"
-    ".type maps_err_str,@function\n"
-    "maps_err_str:\n"
-    ".ascii \"find shared object path (errno=%d)\\n\"\n"
-    ".byte 0x00\n"
-
-    ".globl open_err_str\n"
-    ".type open_err_str,@function\n"
-    "open_err_str:\n"
-    ".ascii \"open file \\\"%s\\\" for reading (errno=%d)\\n\"\n"
-    ".byte 0x00\n"
-
-    ".globl mmap_err_str\n"
-    ".type mmap_err_str,@function\n" 
-    "mmap_err_str:\n"
-    ".ascii \"map file \\\"%s\\\" (errno=%d)\\n\"\n"
-    ".byte 0x00\n"
-
-    ".globl common_err_str\n"
-    ".type common_err_str,@function\n" 
-    "common_err_str:\n"
-    ".ascii \"e9loader error: failed to \"\n"
-    ".byte 0x00\n"
-
-    /*
-     * mmap() error handling code goes here.
-     */
-    ".LMMAP_ERROR:\n"
-    "\tleaq mmap_err_str(%rip), %rdi\n"
-    "\tmov %rax, %rsi\n"
-    "\tneg %rsi\n"
-    "\tjmp e9error\n"
-
-);
-
-static int e9open(const char *filename_0, int flags_0, int mode_0)
-{
-    register uintptr_t filename asm("rdi") = (uintptr_t)filename_0;
-    register uintptr_t flags asm("rsi")    = (uintptr_t)flags_0;
-    register uintptr_t mode asm("rdx")     = (uintptr_t)mode_0;
-    register intptr_t fd asm("rax");
-
-    asm volatile (
-        "mov $2, %%eax\n\t"             // SYS_OPEN
-        "syscall"
-        : "=rax"(fd) : "r"(filename), "r"(flags), "r"(mode) : "rcx", "r11");
-
-    return (int)fd;
-}
-
-static int e9write(int fd_0, const char *buf_0, size_t len_0)
-{
-    register uintptr_t fd asm("rdi")  = (uintptr_t)fd_0;
-    register uintptr_t buf asm("rsi") = (uintptr_t)buf_0;
-    register uintptr_t len asm("rdx") = (uintptr_t)len_0;
-    register intptr_t err asm("rax");
-
-    asm volatile (
-        "mov $1, %%eax\n\t"              // SYS_WRITE
-        "syscall"
-        : "=rax"(err) : "r"(fd), "r"(buf), "r"(len) : "rcx", "r11");
-
-    return (int)err;
-}
-
-static int e9kill(pid_t pid_0, int sig_0)
-{
-    register uintptr_t pid asm("rdi") = (uintptr_t)pid_0;
-    register uintptr_t sig asm("rsi") = (uintptr_t)sig_0;
-    register intptr_t err asm("rax");
-
-    asm volatile (
-        "mov $62, %%eax\n\t"             // SYS_KILL
-        "syscall"
-        : "=rax"(err) : "r"(pid), "r"(sig): "rcx", "r11");
-
-    return (int)err;
-}
-
-static int e9readlink(const char *path_0, char *buf_0, size_t len_0)
-{
-    register uintptr_t path asm("rdi") = (uintptr_t)path_0;
-    register uintptr_t buf asm("rsi")  = (uintptr_t)buf_0;
-    register uintptr_t len asm("rdx")  = (uintptr_t)len_0;
-    register intptr_t err asm("rax");
-
-    asm volatile (
-        "mov $89, %%eax\n\t"             // SYS_READLINK
-        "syscall"
-        : "=rax"(err) : "r"(path), "r"(buf), "r"(len) : "rcx", "r11");
-
-    return (int)err;
-}
-
-static int e9getdents64(int fd_0, struct linux_dirent64 *dirp_0,
-    unsigned count_0)
-{
-    register uintptr_t fd asm("rdi")    = (uintptr_t)fd_0;
-    register uintptr_t dirp asm("rsi")  = (uintptr_t)dirp_0;
-    register uintptr_t count asm("rdx") = (uintptr_t)count_0;
-    register intptr_t len asm("rax");
-
-    asm volatile (
-        "mov $217, %%eax\n\t"           // SYS_GETDENTS64
-        "syscall"
-        : "=rax"(len) : "r"(fd), "r"(dirp), "r"(count) : "rcx", "r11");
-
-    return (int)len;
-}
-
-static int e9close(int fd_0)
-{
-    register uintptr_t fd asm("rdi") = (uintptr_t)fd_0;
-    register intptr_t err asm("rax");
-
-    asm volatile (
-        "mov $3, %%eax\n\t"             // SYS_CLOSE
-        "syscall"
-        : "=rax"(err) : "r"(fd) : "rcx", "r11");
-
-    return (int)err;
-}
-
-/*
- * Convert a number into a string.
- */
-static char *e9num2str(char *str, int x)
-{
-    int r = 1000000000;
-    bool seen = false;
-    while (r != 0)
-    {
-        char c = '0' + x / r;
-        x %= r;
-        r /= 10;
-        if (!seen && c == '0')
-            continue;
-        seen = true;
-        *str++ = c;
-    }
-    if (!seen)
-        *str++ = '0';
-    return str;
-}
-
-/*
- * Print an error message to stderr and abort.
- */
-NO_INLINE NO_RETURN void e9error(const char *err_str, int err)
-{
-    char path_buf[BUFSIZ];
-    int result = e9binary(path_buf);
-    if (result != 0)
-    {
-        path_buf[0] = path_buf[1] = path_buf[2] = '?';
-        path_buf[3] = '\0';
-    }
-    const char *path = path_buf;
-
-    char str_buf[BUFSIZ];
-    char *str = str_buf;
-
-    const char *err_str_0 = common_err_str;
-    while (*err_str_0 != '\0')
-        *str++ = *err_str_0++;
-    while (*err_str != '\0')
-    {
-        if (*err_str == '%')
-        {
-            err_str++;
-            switch (*err_str++)
-            {
-                case 'd':
-                    str = e9num2str(str, err);
-                    break;
-                case 's':
-                    while (*path != '\0')
-                        *str++ = *path++;
-                    break;
-            }
-        }
-        else
-            *str++ = *err_str++;
-    }
-    (void)e9write(STDERR_FILENO, str_buf, str - str_buf);
-    (void)e9kill(/*pid=*/0, SIGABRT);
-    asm volatile ("ud2");
-    __builtin_unreachable();
-}
-
-/*
- * Convert a hex string into a number.
- */
-static intptr_t e9hexstr2num(const char **str_ptr)
-{
-    const char *str = *str_ptr;
-    intptr_t x = 0;
-    while (true)
-    {
-        char c = *str++;
-        if (c >= '0' && c <= '9')
-        {
-            x <<= 4;
-            x |= (intptr_t)(c - '0');
-        }
-        else if (c >= 'a' && c <= 'f')
-        {
-            x <<= 4;
-            x |= (intptr_t)(10 + c - 'a');
-        }
-        else
-        {
-            *str_ptr = str;
-            return x;
-        }
-    }
-}
-
-/*
- * Place the pathname of enclosing binary into `path_buf'.
- */
-static NO_INLINE int e9binary(char *path_buf)
-{
-    // Step (1): Read contents of /proc/self/map_files/
-    int fd = e9open(mapsname, O_RDONLY | O_DIRECTORY, 0);
-    if (fd < 0)
-        return fd;
-    uint8_t buf[BUFSIZ];
-
-    // Step (2): Search for an entry corresponding to the enclosing function.
-    intptr_t code = (intptr_t)&e9binary;
-    bool found = false;
-    while (!found)
-    {
-        int len = e9getdents64(fd, (struct linux_dirent64 *)buf, sizeof(buf));
-        if (len < 0)
-        {
-            e9close(fd);
-            return len;
-        }
-
-        for (int i = 0; !found && i < len; )
-        {
-            struct linux_dirent64 *entry = (struct linux_dirent64 *)(buf + i);
-            i += entry->d_reclen;
-            if (entry->d_type != DT_LNK)
-                continue;
-
-            const char *name = entry->d_name;
-            intptr_t lo = e9hexstr2num(&name);
-            if (code < lo)
-                continue;
-            intptr_t hi = e9hexstr2num(&name);
-            if (code <= hi)
-            {
-                int j;
-                for (j = 0; mapsname[j] != '\0'; j++)
-                    path_buf[j] = mapsname[j];
-                name = entry->d_name;
-                for (int k = 0; name[k] != '\0'; k++)
-                    path_buf[j++] = name[k];
-                path_buf[j] = '\0';
-                found = true;
-            }
-        }
-    }
-    e9close(fd);
-
-    // Step (3): Read the link.
-    // Note: this is necessary since Linux does not allow the path to be
-    //       opened directly.
-    int len = e9readlink(path_buf, path_buf, BUFSIZ);
-    if (len < 0)
-        return len;
-    path_buf[len] = '\0';
-    return 0;
-}
-
-int e9entry(void)
-{
-    char path_buf[BUFSIZ];
-    int err = e9binary(path_buf);
-    if (err != 0)
-        e9error(maps_err_str, -err);
-
-    int fd = e9open(path_buf, O_RDONLY, 0);
-    if (fd < 0)
-        e9error(open_err_str, -fd);
-    return fd;
-}
-

src/e9patch/e9loader.h
@@ -19,9 +19,33 @@
 #ifndef __E9LOADER_H
 #define __E9LOADER_H
 
-/*
- * Loader entry offset.
- */
-#define LOADER_OFFSET           sizeof(void *)
+#define E9_FLAG_EXE                 0x1
+
+struct e9_map_s
+{
+    int32_t  addr;                  // Address (/ PAGE_SIZE)
+    uint32_t offset;                // Offset  (/ PAGE_SIZE)
+    uint32_t size:20;               // Size    (/ PAGE_SIZE)
+    uint32_t __reserved:8;          // Reserved
+    uint32_t r:1;                   // Read?
+    uint32_t w:1;                   // Write?
+    uint32_t x:1;                   // Execute?
+    uint32_t abs:1;                 // Absolute?
+};
+
+struct e9_config_s
+{
+    char     magic[8];              // "E9PATCH\0"
+    uint32_t flags;                 // Flags
+    uint32_t size;                  // Loader total size
+    intptr_t base;                  // Loader base address
+    intptr_t entry;                 // Real entry point
+    intptr_t dynamic;               // DYNAMIC, or 0x0
+    intptr_t mmap;                  // mmap(), or 0x0
+    uint32_t num_maps[2];           // # Mappings
+    uint32_t maps[2];               // Mappings offset
+    uint32_t num_inits;             // # Init functions
+    uint32_t inits;                 // Init functions offset
+};
 
 #endif

src/e9patch/e9loader_elf.cpp
@@ -1,0 +1,341 @@
+/*
+ * e9loader_elf.cpp
+ * Copyright (C) 2021 National University of Singapore
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * NOTE: As a special exception, this file is under the MIT license.  The
+ *       rest of the E9Patch/E9Tool source code is under the GPLv3 license.
+ */
+
+#include <cerrno>
+#include <csignal>
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <syscall.h>
+#include <unistd.h>
+
+#include "e9loader.h"
+
+#define NO_INLINE    __attribute__((__noinline__))
+#define NO_RETURN   __attribute__((__noreturn__))
+
+#define BUFSIZ      8192
+#define PAGE_SIZE   4096
+
+extern "C"
+{
+    void *e9loader(int argc, char **argv, const struct e9_config_s *config);
+    intptr_t e9syscall(long number, ...);
+}
+
+asm (
+    /*
+     * E9Patch loader entry point.
+     */
+    ".globl _entry\n"
+    ".type _entry,@function\n"
+    "_entry:\n"
+    "\tcallq e9loader\n"
+    "\tjmpq *%rax\n"
+
+    ".globl e9syscall\n"
+    ".type e9syscall,@function\n"
+    "e9syscall:\n"
+    "\tmov %edi, %eax\n"
+    "\tmov %rsi, %rdi\n"
+    "\tmov %rdx, %rsi\n"
+    "\tmov %rcx, %rdx\n"
+    "\tmov %r8, %r10\n"
+    "\tmov %r9, %r8\n"
+    "\tmov 0x8(%rsp), %r9\n"
+    "\tsyscall\n"
+    "\tretq\n"
+);
+
+/*
+ * Low-level string formatting routines.
+ */
+static char *e9write_str(char *str, const char *s)
+{
+    while (*s != '\0')
+        *str++ = *s++;
+    return str;
+}
+static char *e9write_char(char *str, char c)
+{
+    *str++ = c;
+    return str;
+}
+static NO_INLINE char *e9write_hex(char *str, uint64_t x)
+{
+    if (x == 0)
+    {
+        *str++ = '0';
+        return str;
+    }
+    bool zero = false;
+    int n = 15;
+    do
+    {
+        unsigned digit = (unsigned)((x >> (n * 4)) & 0xF);
+        n--;
+        if (digit == 0 && !zero)
+            continue;
+        zero = true;
+        if (digit <= 9)
+            *str++ = '0' + digit;
+        else
+            *str++ = 'a' + (digit - 10);
+    }
+    while (n >= 0);
+    return str;
+}
+static NO_INLINE char *e9write_num(char *str, uint64_t x)
+{
+    if (x == 0)
+    {
+        *str++ = '0';
+        return str;
+    }
+    bool zero = false;
+    uint64_t r = 10000000000000000000ull;
+    do
+    {
+        unsigned digit = (unsigned)(x / r);
+        x %= r;
+        r /= 10;
+        if (digit == 0 && !zero)
+            continue;
+        zero = true;
+        *str++ = '0' + digit;
+    }
+    while (r > 0);
+    return str;
+}
+static NO_INLINE char *e9write_format(char *str, const char *msg, va_list ap)
+{
+    char c;
+    while ((c = *msg++) != '\0')
+    {
+        if (c == '%')
+        {
+            c = *msg++;
+            const char *s;
+            uint64_t x;
+            int64_t i;
+            switch (c)
+            {
+                case '%':
+                    str = e9write_char(str, '%');
+                    break;
+                case 'c':
+                    c = (char)va_arg(ap, int);
+                    str = e9write_char(str, c);
+                    break;
+                case 's':
+                    s = va_arg(ap, const char *);
+                    str = e9write_str(str, s);
+                    break;
+                case 'p':
+                    str = e9write_str(str, "0x");
+                    // Fallthrough
+                case 'x': case 'X':
+                    x = (c == 'x'? va_arg(ap, unsigned): va_arg(ap, uint64_t));
+                    str = e9write_hex(str, x);
+                    break;
+                case 'u': case 'U':
+                    x = (c == 'u'? va_arg(ap, unsigned): va_arg(ap, uint64_t));
+                    str = e9write_num(str, x);
+                    break;
+                case 'd': case 'D':
+                    i = (c == 'd'? va_arg(ap, unsigned): va_arg(ap, uint64_t));
+                    if (i < 0)
+                        str = e9write_char(str, '-');
+                    str = e9write_num(str, (uint64_t)(i < 0? -i: i));
+                    break;
+            }
+            continue;
+        }
+        str = e9write_char(str, c);
+    }
+    return str;
+}
+static char *e9write_format(char *str, const char *msg, ...)
+{
+    va_list ap;
+    va_start(ap, msg);
+    str = e9write_format(str, msg, ap);
+    va_end(ap);
+    return str;
+}
+
+/*
+ * Something went wrong, print an error and abort.
+ */
+static NO_INLINE NO_RETURN void e9panic(const char *msg, ...)
+{
+    char buf[BUFSIZ], *str = buf;
+    str = e9write_str(str, "e9patch loader error: ");
+    va_list ap;
+    va_start(ap, msg);
+    str = e9write_format(str, msg, ap);
+    va_end(ap);
+    str = e9write_char(str, '\n');
+
+    size_t len = str - buf;
+    e9syscall(SYS_write, STDERR_FILENO, buf, len);
+    e9syscall(SYS_kill, /*pid=*/0, SIGABRT);
+    while (true)
+        asm volatile ("ud2");
+    __builtin_unreachable();
+}
+
+/*
+ * Write a debug message.
+ */
+static NO_INLINE void e9debug(const char *msg, ...)
+{
+    char buf[BUFSIZ], *str = buf;
+    str = e9write_str(str, "e9patch loader debug: ");
+    va_list ap;
+    va_start(ap, msg);
+    str = e9write_format(str, msg, ap);
+    va_end(ap);
+    str = e9write_char(str, '\n');
+
+    size_t len = str - buf;
+    e9syscall(SYS_write, STDERR_FILENO, buf, len);
+}
+
+/*
+ * Default mmap() routine.
+ */
+static intptr_t e9mmap(void *ptr, size_t len, int prot, int flags, int fd,
+    off_t offset)
+{
+    return e9syscall(SYS_mmap, ptr, len, prot, flags, fd, offset);
+}
+
+typedef intptr_t (*mmap_t)(void *, size_t, int, int, int, off_t);
+typedef void (*init_t)(int, char **, char **, void *);
+
+/*
+ * Load a set of maps.
+ */
+static NO_INLINE void e9load_maps(const e9_map_s *maps, uint32_t num_maps,
+    const uint8_t *elf_base, int fd, mmap_t mmap)
+{
+    for (uint32_t i = 0; i < num_maps; i++)
+    {
+        const uint8_t *addr = (maps[i].abs? (const uint8_t *)NULL: elf_base);
+        addr += (intptr_t)maps[i].addr * PAGE_SIZE;
+        size_t len = (size_t)maps[i].size * PAGE_SIZE;
+        off_t offset = (off_t)maps[i].offset * PAGE_SIZE;
+        int prot = (maps[i].r? PROT_READ: 0x0) |
+                   (maps[i].w? PROT_WRITE: 0x0) |
+                   (maps[i].x? PROT_EXEC: 0x0);
+#if 0
+        e9debug("mmap(addr=%p,size=%U,offset=+%U,prot=%c%c%c)",
+            addr, len, offset,
+            (maps[i].r? 'r': '-'), (maps[i].w? 'w': '-'),
+            (maps[i].x? 'x': '-'));
+#endif
+        intptr_t result = mmap((void *)addr, len, prot, MAP_FIXED | MAP_PRIVATE,
+            fd, offset);
+        if (result < 0)
+            e9panic("mmap(addr=%p,size=%U,offset=+%U,prot=%c%c%c) failed "
+                "(errno=%u)", addr, len, offset,
+                (maps[i].r? 'r': '-'), (maps[i].w? 'w': '-'),
+                (maps[i].x? 'x': '-'), -(int)result);
+    }
+}
+
+/*
+ * Main loader code.
+ */
+void *e9loader(int argc, char **argv, const e9_config_s *config)
+{
+    // Step (0): Sanity checks & initialization:
+    if (config->magic[0] != 'E' || config->magic[1] != '9' ||
+            config->magic[2] != 'P' || config->magic[3] != 'A' ||
+            config->magic[4] != 'T' || config->magic[5] != 'C' ||
+            config->magic[6] != 'H' || config->magic[7] != '\0')
+        e9panic("missing \"E9PATCH\" magic number");
+    const uint8_t *loader_base = (const uint8_t *)config;
+    const uint8_t *loader_end  = loader_base + config->size;
+    const uint8_t *elf_base    = loader_base - config->base;
+
+    // Step (1): Find & open the binary:
+    char buf[BUFSIZ];
+    const char *path = "/proc/self/exe";
+    if ((config->flags & E9_FLAG_EXE) == 0)
+    {
+        // This is a shared object, so use the /proc/self/map_files/
+        // method to find the binary.
+        char *str = buf;
+        str = e9write_format(str, "/proc/self/map_files/%X-%X", loader_base,
+            loader_end);
+        str = e9write_char(str, '\0');
+        path = buf;
+    }
+    ssize_t len = (ssize_t)e9syscall(SYS_readlink, path, buf, sizeof(buf));
+    if (len < 0)
+        e9panic("readlink(path=\"%s\") failed (errno=%u)", buf, -len);
+    buf[len] = '\0';
+    int fd = (int)e9syscall(SYS_open, buf, O_RDONLY, 0);
+    if (fd < 0)
+        e9panic("open(path=\"%s\") failed (errno=%u)", buf, -fd);
+
+    // Step (2): Map in the trampoline code:
+    mmap_t mmap = e9mmap;
+    const struct e9_map_s *maps =
+        (const struct e9_map_s *)(loader_base + config->maps[0]);
+    e9load_maps(maps, config->num_maps[0], elf_base, fd, mmap);
+    if (config->mmap != 0x0)
+        mmap = (mmap_t)(elf_base + config->mmap);
+    maps = (const struct e9_map_s *)(loader_base + config->maps[1]);
+    e9load_maps(maps, config->num_maps[1], elf_base, fd, mmap);
+    e9syscall(SYS_close, fd);
+
+    // Step (3): Call the initialization routines:
+    void *dynamic = NULL;
+    char **envp = NULL;
+    if ((config->flags & E9_FLAG_EXE) != 0)
+    {
+        if (config->dynamic != 0x0)
+            dynamic = (void *)(elf_base + config->dynamic);
+        envp = argv + argc;
+    }
+    const intptr_t *inits = (const intptr_t *)(loader_base + config->inits);
+    for (uint16_t i = 0; i < config->num_inits; i++)
+    {
+        init_t init = (init_t)(elf_base + inits[i]);
+        init(argc, argv, envp, dynamic);
+    }
+
+    // Step (4): Return the entry point:
+    void *entry = (void *)(elf_base + config->entry);
+    return entry;
+}
+

src/e9patch/e9patch.cpp
@@ -526,7 +526,12 @@ void parseOptions(int argc, char * const argv[], bool api)
                 break;
             case OPTION_MEM_LOADER:
                 option_mem_loader = parseIntOptArg("--mem-loader", optarg, 0x0,
-                    INTPTR_MAX);
+                    0x800000000);
+                if (option_mem_loader % PAGE_SIZE != 0)
+                    error("failed to parse argument \"%s\" for the "
+                        "`--mem-loader' option; the loader base address "
+                        "must be a multiple of the page size (%d)", optarg,
+                        PAGE_SIZE);
                 break;
             case OPTION_MEM_MAPPING_SIZE:
                 option_mem_mapping_size = parseIntOptArg("--mem-mapping-size",

commit 840358a4aeeeb397fd10a34ae815b5bd33b73b19

Fix and tell git to ignore build artifacts (#83)
* Fix e9loader artifacts clean-up

Fixes: 9f42bf2c2198 ("Reimplement the E9Patch loader")
Fixes: 812544e1e6779 ("Experimental Windows PE support")

* Tell git to ignore build artifacts

* Cache e9loader artifacts

.gitignore
@@ -1,0 +1,5 @@
+*.bin
+*.o
+src/e9patch/e9loader_*.c
+e9patch
+e9tool

Makefile
@@ -66,23 +66,22 @@ e9patch: $(E9PATCH_OBJS)
 
 clean:
 	rm -rf $(E9PATCH_OBJS) $(E9TOOL_OBJS) e9patch e9tool \
-        src/e9patch/e9loader.c e9loader.out e9loader.o e9loader.bin
+        src/e9patch/e9loader_*.c e9loader_*.o e9loader_*.bin
 
-loader_elf:
+src/e9patch/e9loader_elf.c: src/e9patch/e9loader_elf.cpp
 	$(CXX) -std=c++11 -Wall -fno-stack-protector -Wno-unused-function -fPIC \
-        -Os -c src/e9patch/e9loader_elf.cpp
+        -Os -c $<
 	$(CXX) -pie -nostdlib -o e9loader_elf.bin e9loader_elf.o -T e9loader.ld
-	xxd -i e9loader_elf.bin > src/e9patch/e9loader_elf.c
+	xxd -i e9loader_elf.bin > $@
 
-loader_pe:
+src/e9patch/e9loader_pe.c: src/e9patch/e9loader_pe.cpp
 	$(CXX) -std=c++11 -Wall -fno-stack-protector -fno-zero-initialized-in-bss \
-        -Wno-unused-function -fPIC -mabi=ms -fshort-wchar \
-        -Os -c src/e9patch/e9loader_pe.cpp
+        -Wno-unused-function -fPIC -mabi=ms -fshort-wchar -Os -c $<
 	$(CXX) -pie -nostdlib -o e9loader_pe.bin e9loader_pe.o -T e9loader.ld
-	xxd -i e9loader_pe.bin > src/e9patch/e9loader_pe.c
+	xxd -i e9loader_pe.bin > $@
 
-src/e9patch/e9elf.o: loader_elf
-src/e9patch/e9pe.o: loader_pe
+src/e9patch/e9elf.o: src/e9patch/e9loader_elf.c
+src/e9patch/e9pe.o: src/e9patch/e9loader_pe.c
 
 contrib/zydis/libZydis.a:
 	(cd contrib/zydis/; make)